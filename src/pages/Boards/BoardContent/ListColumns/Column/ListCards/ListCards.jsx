840688d0a564261d5da9071b0a0846485a60a5b2
phieu37 <86352994+phieu37@users.noreply.github.com>
2023-12-30 20:29:08 +0700

    trello_board_content_p2

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,34 @@
+import Box from '@mui/material/Box'
+import Card from './Card/Card'
+
+function ListCards() {
+  return (
+    // Box Column List card, 1 spacing = 8
+    <Box sx={{
+      p: '0 5px',
+      m: '0 5px',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 1,
+      overflowX: 'hidden',
+      overflowY: 'auto',
+      maxHeight: (theme) => `calc(
+                  ${theme.trello.boardContentHeight} -
+                  ${theme.spacing(5)} -
+                  ${theme.trello.columnHeaderHeight} -
+                  ${theme.trello.columnFooterHeight}
+                  )`,
+      '&::-webkit-scrollbar-thumb': { background: '#ced0da' },
+      '&::-webkit-scrollbar-thumb:hover': { background: '#bfc2cf' }
+    }}>
+      <Card />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+      <Card temporaryHideMedia />
+    </Box>
+  )
+}
+
+export default ListCards

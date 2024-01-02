ef885d114bd823509c380b6f97d3b1933811fc3b
phieu37 <86352994+phieu37@users.noreply.github.com>
2024-01-02 10:44:00 +0700

    axios_call_api

---
+++ src/apis/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { API_ROOT } from '~/utils/constants'
+
+// Lưu ý: Đối với việc sử dụng axios
+// Tất cả các funtion bên dưới mình chỉ request và lấy data từ response luôn
+// mà ko try catch hay then catch để bắt lỗi. Lý do ở phía FE ko cần thiết làm vậy
+// đối với mọi request bởi nó sẽ gây ra việc dư thừa code catch lỗi quá nhiều
+// Giải pháp Clean Code gọn gàng là sẽ catch lỗi tập trung tại 1 nơi,
+// tận dụng Interceptors trong axios (khóa nâng cao)
+
+export const fetchBoardDetailsAPI = async (boardId) => {
+  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+  // Lưu ý: axios sẽ trả kết quả về qua property của nó là data
+  return response.data
+}
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,29 @@
 // Board Detail
+import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  // Tạm thời fix cứng boardId, flow chuẩn chỉnh về sau khi học nâng cao trực tếp
+  // sẽ sử dụng react-router-dom để lấy chuẩn boardId từ URL về
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    const boardId = '657fcd491071abe51ea669cb'
+    // Call API
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
